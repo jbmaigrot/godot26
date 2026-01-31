12ca3a170be156987b6b7c69c01752087a804bd8
jbmaigrot <jb.maigrot@gmail.com>
2026-01-31 17:08:44 +0100

    tower records enemies in range

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -1,6 +1,8 @@
 extends CharacterBody2D
 class_name Enemy
 
+@export var speed = 50
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,4 +10,16 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	#(temp/debug) control the enemy
+	var moveDirection = Vector2()
+	if (Input.is_key_pressed(KEY_LEFT)):
+		moveDirection.x -= 1
+	if (Input.is_key_pressed(KEY_RIGHT)):
+		moveDirection.x += 1
+	if (Input.is_key_pressed(KEY_UP)):
+		moveDirection.y -= 1
+	if (Input.is_key_pressed(KEY_DOWN)):
+		moveDirection.y += 1
+	
+	velocity = moveDirection * speed
+	move_and_slide()
--- Scripts/tower.gd
+++ Scripts/tower.gd
@@ -3,28 +3,42 @@ class_name Tower
 
 @export var projectile_scene : PackedScene
 
+@export var shoot_cooldown = 2
+var current_cooldown = 0
+
+var target :Enemy = null
+var enemies_in_range = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
-
-func _on_area_entered(area):
-	print("An area overlapped me!")
-	print(area.get_class())
+	if (current_cooldown > 0):
+		current_cooldown = current_cooldown - delta
+	else:
+		if !(target in enemies_in_range):
+			target = null
+		if (!target && enemies_in_range.size()!=0):
+			target = enemies_in_range[0]
+		if (target):
+			shoot()
+			current_cooldown = shoot_cooldown
 
 func _on_body_entered(body):
-	print("A body overlapped me!")
-	print(body.get_class())
-	
 	if body is Enemy:
-		print("AAAAAAAH")
+		enemies_in_range.append(body)
+		
+func _on_body_exited(body):
+	if body is Enemy:
+		enemies_in_range.erase(body)
+
+func shoot():
 		var projectile :Projectile = projectile_scene.instantiate()
 		owner.add_child(projectile)
 		
+		# projectile spawns on tower and targets incoming enemy (body)
 		projectile.position = position
-		projectile.target = body
+		projectile.target = target
+	
